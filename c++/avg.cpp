3a87ed151e33a0422fc1b805daf6ddd223440d5d
RAHUL KUMAR <rahulpbps2011@gmail.com>
2022-10-14 11:42:30 +0530

    Average Of Three Number

---
+++ c++/avg.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three number:";
+    cin>>a>>b>>c;
+    cout<<"Average of three number ::"<<((a+b+c)/3)<<endl;
+    return 0;
+}
